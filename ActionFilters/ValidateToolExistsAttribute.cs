6983219b2dc20b68b6571d1f40a1c318f4eca84f
Felipe Kosouski <felipe.kosouski@gmail.com>
2020-05-31 21:30:05 -0300

    Added validation filters

---
+++ ActionFilters/ValidateToolExistsAttribute.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Vuttr.API.Domain.Repository;
+using Vuttr.API.LoggerService;
+
+namespace Vuttr.API.ActionFilters
+{
+    public class ValidateToolExistsAttribute : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public ValidateToolExistsAttribute(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var trackChanges = context.HttpContext.Request.Method.Equals("PUT") ? true : false;
+            var id = (Guid)context.ActionArguments["id"];
+            var tool = await _repository.Tool.GetToolAsync(id, trackChanges);
+            if (tool == null) 
+            {
+                _logger.LogInfo($"Tool with id: {id} doesn't exist in the database.");
+                context.Result = new NotFoundResult(); 
+            }
+            else
+            {
+                context.HttpContext.Items.Add("tool", tool); await next();
+            }
+        }
+    }
+}
--- Domain/DTO/Tool/ToolForManipulationDto.cs
+++ Domain/DTO/Tool/ToolForManipulationDto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Vuttr.API.Domain.DTO.Tool
 {
     public class ToolForManipulationDto
     {
+        [Required(ErrorMessage = "Tool {0} is a required field")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the {0} field is {1} characters")]
         public string Title { get; set; }
+        
+        [MaxLength(255, ErrorMessage = "Maximum length for the {0} field is {1} characters")]
         public string Description { get; set; }
+        
+        [MaxLength(100, ErrorMessage = "Maximum length for the {0} field is {1} characters")]
         public string Link { get; set; }
+        
         public string[] Tags { get; set; }
     }
 }
